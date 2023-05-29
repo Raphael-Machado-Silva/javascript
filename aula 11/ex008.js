9a1b21b662ca4998bcb82166b6386216d60b6f30
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-05-29 16:16:45 -0300

    Update ex009.js

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,6 +1,9 @@
+//Condição Simples, só tendo o if
 let vel = 90
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
 if (vel > 60){
-    console.log('Voçê ultrapassou a velocidade máxima permitida. MULTADO!')
+    console.log('Voçê ultrapassou a velocidade máxima permitida de 60km/h. MULTADO!')
 }
+
 console.log('Dirija sempre usando o cinto de segurança!')

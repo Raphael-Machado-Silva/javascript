9452182918e78a16ba650b9191da0aa458d271ee
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-05-29 16:09:32 -0300

    Update ex008.js

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+let vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Voçê ultrapassou a velocidade máxima permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')

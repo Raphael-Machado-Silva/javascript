65b7e7f2f9ef220304258765b7e9b264bb2ba91c
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-05 15:52:44 -0300

    Update ambiente.js

--- aula 13/node_4e30edfae6070.tmp.js
+++ aula 13/node_4e30edfae6070.tmp.js
@@ -9,12 +9,11 @@ console.log('Tudo Bem?') */
 let c = 1
 // while = enquanto ---- do = faça
 
-while (c <= 6){ // (teste lógico no início)
+/*while (c <= 6){ // (teste lógico no início)
     console.log(`Passo ${c}!`)
     c++ // c = c+1
-} 
+} */
 
-do{ //Teste lógico no final
+do{
     console.log(`Passo ${c}!!!`)
-    c++
 } while (c <= 6)

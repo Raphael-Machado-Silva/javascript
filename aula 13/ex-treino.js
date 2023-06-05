906914050e4fb819b330d58950bfd20802b6510e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-05 15:59:50 -0300

    Update ex-treino.js

--- aula 13/ex-treino.js
+++ aula 13/ex-treino.js
@@ -1,6 +1,6 @@
 var idade = 10
 
-while (idade <= 17){
+while (idade <= 17){ // ao chegar em 17 a variavel ainda aumenta seu valor em 1, chegando ao 18 no console.log final
     console.log(`Você ainda tem ${idade} anos, logo você é menor de idade!`)
     idade++
 } console.log(`Agora você possui ${idade} anos, logo é maior de idade`)

249717b21839debe0f8707b471946f282c0ccefd
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-05 16:03:01 -0300

    Update ex-treino.js

--- aula 13/ex-treino.js
+++ aula 13/ex-treino.js
@@ -1,6 +1,9 @@
+
+
 var idade = 10
+const sexo = 'masculino' //lembrar de usar aspas
 
 while (idade <= 17){ // ao chegar em 17 a variavel ainda aumenta seu valor em 1, chegando ao 18 no console.log final
-    console.log(`Você ainda tem ${idade} anos, logo você é menor de idade!`)
+    console.log(`Você ainda tem ${idade} anos e possui sexo ${sexo}, logo você é menor de idade!`)
     idade++
-} console.log(`Agora você possui ${idade} anos, logo é maior de idade`)
+} console.log(`Agora você possui ${idade} anos e possui sexo ${sexo}, logo é maior de idade`)

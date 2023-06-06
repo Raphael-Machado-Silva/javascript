5ba49fb97449e6da4ba46dc1e30fedc00dc800dd
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-06 15:26:15 -0300

    Update variáveis_controle.js

--- aula 14/variáveis_controle.js
+++ aula 14/variáveis_controle.js
@@ -1,6 +1,12 @@
 var c = 1
 
+/*Estrutura de repetição sem variável de controle
+--------------------------------------------------------
 while (c<=10){
-    console.log(`Você possui ${c}anos agora!`)
+    console.log(`Você possui ${c} anos agora!`)
     c++
+}*/
+
+for (var c=1; c<=10; c++){ // início; teste; incremento
+    console.log(`Agora você possui ${c} anos de idade!`)
 }

ff12aa03f2a7fee2f2a321a8a16b4b510af78d46
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-06 15:41:58 -0300

    Update ambiente.js

--- aula 14/ambiente.js
+++ aula 14/ambiente.js
@@ -6,3 +6,5 @@ for (let c = 1; c <= 5; c++){
     console.log(c)
 }
 console.log('Fim!')
+
+/* Resumo de variáveis de controle: é um modo de declarar uma repetição usando a tag FOR seguida de (), onde dentro das mesmas será inserido 1- a variavel a ser explorada ; 2- o teste a ser feito, ou seja, a afirmação ; 3- o que deverá ocorrer ao termino do teste, como por exemplo: adicionar um valor a variavel, mudar seu valor, dentre outros...

3b3ce6ae00626762f586410b2ed10c91e14eccef
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-09 10:45:49 -0300

    Update ambiente.js

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,6 +1,8 @@
-
+// ----VETORES EM JAVASCRIPT (VARIAVEIS COMPOSTAS)----
 
 let num = [5, 8, 2, 9, 3]
+
+num[5]= 10 // para adicionar mais um valor a variavel
 
 console.log(`Nossa variável é: ${num}`)
 

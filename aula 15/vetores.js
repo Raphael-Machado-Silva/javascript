51312d29a1c4e71d96860cda111d77cd3317f029
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-09 11:04:23 -0300

    Create vetores.js

---
+++ aula 15/vetores.js
@@ -0,0 +1,11 @@
+//Usando repetição para mostrar os valores do vetor num
+
+
+let valores = [8, 1, 7, 4, 2, 9]
+
+valores.sort()
+
+// Código para alterar o indice do vetor e mostrar todos de maneira seguida e crescente
+for(let pos=0; pos < valores.length; pos++){
+    console.log(valores[pos])
+}

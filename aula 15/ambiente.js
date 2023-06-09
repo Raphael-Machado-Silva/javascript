232b707338af324d5d26d1203091f1b00366371d
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-09 10:54:28 -0300

    Update ambiente.js

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,6 +1,6 @@
 // ----VETORES EM JAVASCRIPT (VARIAVEIS COMPOSTAS)----
 
-let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3] 
 
 num[5]= 0 // para adicionar mais um valor a variavel ou usar o:
 num.push(7) // serve para adicionar mais um elemento, assim como o de cima
@@ -11,3 +11,4 @@ console.log(`Nossa variável é: ${num}`)
 console.log(`O meu elemento que ocupa a posição 1 é: ${num[1]}!`)
 console.log(`Meu vetor possui: ${num.length} elementos!`)
 
+//---------------OBS: Vetores usam [] e não {} ----------------

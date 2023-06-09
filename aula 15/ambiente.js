5e8832ac06791fb89f1691498697e8b9625b60e1
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-06-09 10:50:46 -0300

    Update ambiente.js

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -2,7 +2,10 @@
 
 let num = [5, 8, 2, 9, 3]
 
-num[5]= 10 // para adicionar mais um valor a variavel
-
+num[5]= 0 // para adicionar mais um valor a variavel ou usar o:
+num.push(7) // serve para adicionar mais um elemento, assim como o de cima
+num.sort() // colocar em ordem crescente
+num.length // esta tag serve para mostrar quantos elementos este vetor possui
 console.log(`Nossa variável é: ${num}`)
+console.log(num.length)
 

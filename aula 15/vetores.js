35915d2b298c4406c5ff50b996a17efae144f8a4
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-19 11:24:17 -0300

    Update vetores.js

--- aula 15/vetores.js
+++ aula 15/vetores.js
@@ -10,11 +10,14 @@ for(let pos=0; pos < valores.length; pos++){
     console.log(`A posição ${pos} possui o valor: ${valores[pos]}!`)
 }
 
+
 // Maneira mais simples de fazer o exemplo acima
 for(let pos in valores){
     console.log(valores[pos])
 }
 
-//Para buscar um valor dentro de um vetor usar a tag: valores.indexOf(valor a ser procurado), neste caso você pergunta se tem o número que está dentro do () e ele te retorna o indice q o mesmo se encontra
+
 
 console.log(`O valor 4 está na posição: ${valores.indexOf(4)}`)
+
+//Para buscar um valor dentro de um vetor usar a tag: valores.indexOf(valor a ser procurado), neste caso você pergunta se tem o número que está dentro do () e ele te retorna o indice q o mesmo se encontra

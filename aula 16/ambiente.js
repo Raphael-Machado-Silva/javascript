62904fa26afaf8e7a4308f71a486d01b383ada1e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-20 15:04:04 -0300

    Update ambiente.js

--- aula 16/ambiente.js
+++ aula 16/ambiente.js
@@ -1,1 +1,12 @@
 //  -------------------------------Funções------------------------------
+
+function parimp(n){
+    if(n%2==0){
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+let res = parimp(27)
+
+console.log(res)

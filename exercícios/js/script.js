c6e20dd50c93f4aa54c71749e01b8dbd29df5a1e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-24 09:34:14 -0300

    Update

--- exercícios/js/script.js
+++ exercícios/js/script.js
@@ -20,7 +20,10 @@ function isLista (n, l){ //verificar se o valor está na lista
 }
 function adicionar(){ //verificar se é um número, e se ele não está na lista a ação que deve ser feita é inseri-lo na lista
     if(isNumero(num.value) && !isLista(num.value, valores)){
-        valores.push(num.value);
+        valores.push(Number(num.value));
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item);
     } else{
         window.alert("Valor inválido ou já encontrado na lista!")
     }

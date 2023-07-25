003b9c1ed3694f6f98adcb8d884e7cac4b3fdd36
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-25 09:18:22 -0300

    Create funcoes.js

---
+++ funções/funcoes.js
@@ -0,0 +1,11 @@
+/*
+    Função: Trecho de código que só é executado quando é chamado.
+
+        Função Void (vazia)
+        Função com Parâmetros (s)
+        Função Return 
+        Função Arrow / Arrow Function
+
+*/
+
+function incremento

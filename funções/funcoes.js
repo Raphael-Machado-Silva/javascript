72b04d04336b2d04f7198b3ee20bb49b8ddd3a05
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-25 09:30:45 -0300

    Update funcoes.js

--- funções/funcoes.js
+++ funções/funcoes.js
@@ -6,20 +6,28 @@
         Função Return 
         Função Arrow / Arrow Function
 
-*/
 
-/*
 ---------------------Função Vazia---------------------------
 let valor = 20
 function incremento(){
         valor = 20 + 30
 }
 incremento()
-console.log(valor) */
+console.log(valor) 
 
+------------------Função com Parâmetros---------------------------
 
 function mostraNome(nome){
     console.log(nome)
 }
 
 mostraNome('Rodolfo')
+mostraNome('Raphael')
+*/
+
+
+function soma(n1, n2){
+    const somaDosNumeros = n1 + n2;
+    console.log(somaDosNumeros)
+}
+soma(5,8)

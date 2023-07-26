0f92b27c896a05c5cc3cfef35c01ad66ffe51ce2
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-26 09:31:16 -0300

    Update funcao_arrow.js

--- funções/funcao_arrow.js
+++ funções/funcao_arrow.js
@@ -5,3 +5,11 @@ const multi = (n1, n2)  => {
 
 const res = multi(5,6)
 console.log(res)
+//----------------------------------------------------------------
+const soma = (n1, n2)  => {
+    const adicao = n1 + n2;
+    return adicao
+}
+
+const result = soma(5,6)
+console.log(result)

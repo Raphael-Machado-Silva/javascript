26c081ce5aafe1fe863a4333cf334c2337e27c35
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-27 11:16:48 -0300

    Update função_parametros.js

--- funções/função_parametros.js
+++ funções/função_parametros.js
@@ -1,10 +1,10 @@
 //-----------------Funções com Parâmetros-------------------
 
-function multi = (n1, n2)  => {
+function multi (n1, n2) {
     const multiplicaçao = n1 * n2;
     return multiplicaçao
 }
 
 const res = multi(5,6)
-console.log(res)
+
 //

f6bc8a3cab032728d436cfff6646da9e1ca17332
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-27 11:39:02 -0300

    Update função_parametros.js

--- funções/função_parametros.js
+++ funções/função_parametros.js
@@ -9,3 +9,11 @@ const res = multi(5,6)
 console.log(res)
 
 //----------------------------------------------------------------
+
+function divisao (n1, n2){
+    const div = n1 / n2;
+    return div
+}
+
+const result = divisao(5,6)
+console.log(result)
